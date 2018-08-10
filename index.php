76c766a7f8f45e0efe441ec09d4812fd680ce3ff
ArnaudBesnard <besnard.arno@gmail.com>
2018-08-10 19:01:02 +0200

    Ajout bootstrap

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
 <body>
     <!--Feuille de style générale et responsive-->
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 </body>
+<header>
+    <div class='titre'>Un billet simple pour l'Alaska</div>
+</header>
 <?php 
     require('_connect/connect.php'); 
     include('view.php');
