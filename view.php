4fed94666d12eec3555c6c8b3589ae109e048639
ArnaudBesnard <besnard.arno@gmail.com>
2018-08-10 21:52:16 +0200

    Ajout menu + pages add et edit billet

--- view.php
+++ view.php
@@ -1,23 +1,36 @@
 <?php
 $reponse = $bdd->query('SELECT * FROM billet');
-while ($donnees = $reponse->fetch())
-{
 ?>
-    <div class="container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-2">
+                <nav>
+                    <ul>
+                        <li><a href="index.php">Accueil</a></li>
+                        <li><a href="edit_billet.php">Editer un billet</a></li>
+                        <li><a href="add_billet.php">Ajouter un billet</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-10">
+                <?php
+                while ($donnees = $reponse->fetch())
+                { ?>
+                    <h1>
+                        <?php echo($donnees['titre']); ?>
+                    </h1>
+                    <div class='date_billet'>
+                        <?php echo($donnees['date']); ?> </div>
+                    <div class='contenu'>
+                        <?php echo($donnees['contenu']); ?>
+                    </div>
+                    <div class='auteur'>
+                        <?php echo($donnees['auteur']); ?>
+                    </div>
+                    <button type="button" class="btn btn-warning">Editer</button>
+                    <button type="button" class="btn btn-danger">Supprimer</button>
 
-        <h1>
-            <?php echo($donnees['titre']); ?>
-        </h1>
-        <div class='date'>
-            <?php echo($donnees['date']); ?> </div>
-        <div class='contenu'>
-            <?php echo($donnees['contenu']); ?>
+                    <?php } ?>
+            </div>
         </div>
-        <div class='auteur'>
-            <?php echo($donnees['auteur']); ?>
-        </div>
-        <button type="button" class="btn btn-warning">Editer</button>
-        <button type="button" class="btn btn-danger">Supprimer</button>
-
-        <?php } ?>
     </div>
