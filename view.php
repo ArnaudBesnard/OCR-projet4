1e67395718e96ab341a5564efa6e8c490ce7ef32
ArnaudBesnard <besnard.arno@gmail.com>
2018-08-10 20:04:00 +0200

    Modif affichage billet

--- view.php
+++ view.php
@@ -1,13 +1,23 @@
 <?php
 $reponse = $bdd->query('SELECT * FROM billet');
-$donnees = $reponse->fetch();
+while ($donnees = $reponse->fetch())
+{
 ?>
+    <div class="container">
 
-    <h1>
-        <?php echo($donnees['titre']); ?>
-    </h1>
-    <div class='date'><?php echo($donnees['date']); ?> </div>
-    <div class='contenu'>
-        <?php echo($donnees['contenu']); ?>
+        <h1>
+            <?php echo($donnees['titre']); ?>
+        </h1>
+        <div class='date'>
+            <?php echo($donnees['date']); ?> </div>
+        <div class='contenu'>
+            <?php echo($donnees['contenu']); ?>
+        </div>
+        <div class='auteur'>
+            <?php echo($donnees['auteur']); ?>
+        </div>
+        <button type="button" class="btn btn-warning">Editer</button>
+        <button type="button" class="btn btn-danger">Supprimer</button>
+
+        <?php } ?>
     </div>
-    <div class='auteur'><?php echo($donnees['auteur']); ?></div>
